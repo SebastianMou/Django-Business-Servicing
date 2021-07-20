76a29894de7ed5e463c4f6bac352b4e4553bd495
SebastianMou <Sebastian_Mou@protonmail.com>
2021-07-20 17:38:08 -0500

    Integrating Bootstrap to the Project v2 - P1

--- Shop/views.py
+++ Shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 # Create your views here.
+
+def all_products(request):
+    products = Product.objects.all() # query: storing all od the data inside this veriable
+    return render(request, 'Shop/home.html', {'products': products})
+    
+
+
