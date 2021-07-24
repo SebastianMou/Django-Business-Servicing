706f5336436ed646d64c5564a236cf0086bebff6
SebastianMou <Sebastian_Mou@protonmail.com>
2021-07-24 14:52:31 -0500

    creating the detail html page

--- Shop/urls.py
+++ Shop/urls.py
@@ -5,4 +5,5 @@ app_name = 'Shop'
 
 urlpatterns = [
     path('', views.all_products, name='all_products'),
+    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -1,11 +1,19 @@
+from django.shortcuts import get_list_or_404
 from django.shortcuts import render
 from .models import Category, Product
 
+
 # Create your views here.
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
 
 def all_products(request):
     products = Product.objects.all() # query: storing all od the data inside this veriable
     return render(request, 'Shop/home.html', {'products': products})
     
-
-
+def product_detail(request, slug):
+    product = get_list_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'Shop/products/detail.html', {'product': product})
