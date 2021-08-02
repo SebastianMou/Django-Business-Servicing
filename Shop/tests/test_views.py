89697c2ac2ce8c1b4044a3cbaeacb06bf7709505
SebastianMou <Sebastian_Mou@protonmail.com>
2021-08-02 17:03:50 -0500

    session V table

---
+++ Shop/tests/test_views.py
@@ -0,0 +1,16 @@
+from unittest import skip
+from django.contrib.auth.models import User
+from django.http import HttpRequest
+from django.test import Client, RequestFactory, TestCase
+from django.urls import reverse
+
+from store.models import Category, Product
+from store.views import product_all
+
+class TestViewResponses(TestCase):
+
+    def test_url_allowed_hosts(self):
+        response = self.c.get('/', HTTP_HOST='noaddress.com')
+        self.assertEqual(response.status_code, 400)
+        response = self.c.get('/', HTTP_HOST='yourdomain.com')
+        self.assertEqual(response.status_code, 200)
